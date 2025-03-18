87cd287874a7841893b35decbef3bfca64b373aa
Kerby Keith Aquino <hello@kurojifusky.com>
2025-03-18 11:06:02 +0800

    make sure keygen is relative to project dir
    - added simple dockerfile (not tested yet)

--- server/generate_key.go
+++ server/generate_key.go
@@ -6,50 +6,44 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
+	"regexp"
 )
 
 func main() {
 	bytes := make([]byte, 48)
-
 	_, err := rand.Read(bytes)
-	if err != nil {
-		fmt.Println("Error generating token:", err)
-		return
-	}
 
 	tokenToFeed := "om_" + hex.EncodeToString(bytes)
 
-	// Define .env file path
-	envFilePath := filepath.Join(filepath.Dir(os.Args[0]), "..", ".env")
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting current working directory:", err)
+		return
+	}
+
+	envFilePath := filepath.Join(cwd, "..", ".env")
 
 	var envFileContent string
 
-	if _, err := os.Stat(envFilePath); err == nil {
-		data, err := os.ReadFile(envFilePath)
-		if err != nil {
-			fmt.Println("Error reading .env file:", err)
-			return
-		}
-
+	if data, err := os.ReadFile(envFilePath); err == nil {
 		envFileContent = string(data)
+	} else if !os.IsNotExist(err) {
+		fmt.Println("Error reading .env file:", err)
+		return
+	}
+
+	re := regexp.MustCompile(`(?m)^API_KEY=.*$`)
+
+	if re.MatchString(envFileContent) {
+		envFileContent = re.ReplaceAllString(envFileContent, "API_KEY="+tokenToFeed)
 	} else {
-		file, err := os.Create(envFilePath)
-		if err != nil {
-			fmt.Println("Error creating .env file:", err)
-			return
+		if len(envFileContent) > 0 {
+			envFileContent += "\n"
 		}
-
-		defer file.Close()
+		envFileContent += "API_KEY=" + tokenToFeed
 	}
 
-	if strings.Contains(envFileContent, "API_KEY=") {
-		envFileContent = strings.ReplaceAll(envFileContent, `API_KEY=.*`, "API_KEY="+tokenToFeed)
-	} else {
-		envFileContent += "\nAPI_KEY=" + tokenToFeed
-	}
-
-	err = os.WriteFile(envFilePath, []byte(strings.TrimSpace(envFileContent)), 0644)
+	err = os.WriteFile(envFilePath, []byte(envFileContent), 0644)
 	if err != nil {
 		fmt.Println("Error writing to .env file:", err)
 		return
