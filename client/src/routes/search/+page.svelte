0cb620e7d6db438f431bba9deb12fd1afed525b5
Kerby Keith Aquino <hello@kurojifusky.com>
2025-03-21 10:11:25 +0800

    don't burn my retinas
    - add search route

---
+++ client/src/routes/search/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { Meta } from "$lib/components";
+  import InputField from "$lib/components/InputField.svelte";
+  import type { PageData } from "./$types";
+
+  let { data }: { data: PageData } = $props();
+</script>
+
+<Meta title="Search page" />
+<div class="min-h-[90dvh] flex justify-center items-start">
+  <div>
+    <InputField />
+  </div>
+</div>
